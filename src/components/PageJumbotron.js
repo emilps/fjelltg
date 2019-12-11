fca12fb294259d9320ba9f25610b86bc50faa866
Eirik Dahlen <dahleneirik@gmail.com>
2019-12-11 19:42:34 +0100

    style: add styling to jumbotron

--- src/components/PageJumbotron.js
+++ src/components/PageJumbotron.js
@@ -3,24 +3,26 @@ import PropTypes from "prop-types";
 
 const PageJumbotron = ({ title, image, description }) => {
   return (
-    <div className="container">
+    <div className="jumbotron-container">
       <div
         className="full-width-image-container-margin-top-0"
         style={{
           backgroundImage: `url('/img/blog-index.jpg')`
         }}
       >
-        <h1
-          className="has-text-weight-bold is-size-1"
-          style={{
-            color: "white",
-            padding: "1rem"
-          }}
-        >
-          {title}
-        </h1>
-        <hr className="hr-jumbotron" />
-        <h3>{description}</h3>
+        <div className="jumbotron-centered-elements">
+          <h1
+            className="has-text-weight-bold is-size-1"
+            style={{
+              color: "white",
+              padding: "1rem"
+            }}
+          >
+            {title}
+          </h1>
+          <hr className="hr-jumbotron" />
+          <h3>{description}</h3>
+        </div>
       </div>
     </div>
   );
