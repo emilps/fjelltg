0447a691d56f443e1344776d36f8892c8ba9fb94
Emil Schrøder <emilschroder@Emils-MacBook-Pro.local>
2020-01-01 22:55:14 +0100

    Design footer according to sketches

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,22 +8,33 @@ const Footer = class extends React.Component {
     return (
       <footer className="footer has-text-white-ter">
         <div className="content has-text-white-ter">
-          <div className="column has-text-centered">
-            <img
-              src={logo}
-              alt="Fjell Technology Group AS"
-              style={{ width: '14em', height: '10em' }}
-            />
-          </div>
           <div className="container has-text-white-ter">
             <div className="columns">
-              <div className="column is-4">
+              <div className="column is-4 has-text-centered-mobile">
+                <img
+                  src={logo}
+                  alt="Fjell Technology Group AS"
+                  style={{ width: '14em', marginTop: '5px' }}
+                />
+                <p style={{ marginTop: '20px' }}>
+                  Your partner <br />
+                  <span className="has-text-weight-bold">
+                    for mass and heat transfer technology
+                  </span>
+                </p>
+              </div>
+              <div className="column is-3 has-text-centered-mobile">
                 <section className="menu">
                   <p className="has-text-weight-bold has-text-left">Explore</p>
                   <ul className="menu-list remove-margin">
                     <li>
                       <Link to="/" className="navbar-item remove-padding">
                         Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item remove-padding" to="/about">
+                        About
                       </Link>
                     </li>
                     <li>
@@ -43,21 +54,11 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item remove-padding" to="/blog">
-                        WasteWater
-                      </Link>
-                    </li>
-                    <li>
                       <Link
                         className="navbar-item remove-padding"
-                        to="/contact/examples"
+                        to="/solutions"
                       >
-                        Protein Recycling
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item remove-padding" to="/about">
-                        About
+                        Solutions
                       </Link>
                     </li>
                     <li>
@@ -71,19 +72,24 @@ const Footer = class extends React.Component {
                   </ul>
                 </section>
               </div>
-              <div className="column is-4">
+              <div className="column is-3 has-text-centered-mobile">
                 <section>
                   <p className="has-text-weight-bold">Visit</p>
                   <p className="has-text-weight-light">
-                    Main office: Idrettsvegen 103 N5353 Straume
+                    Main office: <br /> Idrettsvegen 103 <br />
+                    N5353 Straume
                   </p>
                   <p className="">
-                    GreenTech office: Kong Christian Frederiks Plass 3 N-5006
-                    Bergen
+                    GreenTech office:
+                    <br /> Kong Christian Frederiks Plass 3 <br />
+                    N-5006 Bergen
                   </p>
                 </section>
               </div>
-              <div className="column is-4">
+              <div
+                className="column is-3 has-text-centered-mobile	
+"
+              >
                 <section className="menu">
                   <p className="has-text-weight-bold">Follow</p>
                   <ul className="menu-list remove-margin">
@@ -91,7 +97,7 @@ const Footer = class extends React.Component {
                       <a
                         title="linkedin"
                         href="https://www.linkedin.com/company/fjell-technology-group-as/"
-                        className="remove-padding"
+                        className="remove-padding navbar-item"
                       >
                         LinkedIn
                       </a>
@@ -100,7 +106,7 @@ const Footer = class extends React.Component {
                       <a
                         title="facebook"
                         href="https://www.facebook.com/FjellGreenTech/"
-                        className="remove-padding"
+                        className="remove-padding navbar-item"
                       >
                         Facebook
                       </a>
@@ -109,7 +115,7 @@ const Footer = class extends React.Component {
                       <a
                         title="instagram"
                         href="https://www.instagram.com/fjelltechnology/"
-                        className="remove-padding"
+                        className="remove-padding navbar-item"
                       >
                         Instagram
                       </a>
@@ -118,7 +124,7 @@ const Footer = class extends React.Component {
                       <a
                         title="twitter"
                         href="https://twitter.com"
-                        className="remove-padding"
+                        className="remove-padding navbar-item"
                       >
                         Twitter
                       </a>
@@ -126,6 +132,9 @@ const Footer = class extends React.Component {
                   </ul>
                 </section>
               </div>
+            </div>
+            <div style={{ marginTop: '7rem' }}>
+              <p>© 2020 Fjell Technology Group. All rights reserved.</p>
             </div>
           </div>
         </div>
