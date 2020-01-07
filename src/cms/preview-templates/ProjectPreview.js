01a3552c286d8f7b66aa427adb9e1d0f1563522c
Emil Schrøder <emilschroder@Emils-MacBook-Pro.local>
2020-01-07 01:39:21 +0100

    Preview images

--- src/cms/preview-templates/ProjectPreview.js
+++ src/cms/preview-templates/ProjectPreview.js
@@ -2,22 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ProjectTemplate } from '../../templates/project';
 
-const ProjectPreview = ({ entry, widgetFor }) => (
-  <ProjectTemplate
-    description={entry.getIn(['data', 'description'])}
-    title={entry.getIn(['data', 'title'])}
-    seconddescription={entry.getIn(['data', 'seconddescription'])}
-    secondtitle={entry.getIn(['data', 'secondtitle'])}
-    thirddescription={entry.getIn(['data', 'thirddescription'])}
-    thirdtitle={entry.getIn(['data', 'thirdtitle'])}
-  />
-);
+const ProjectPreview = ({ entry }) => {
+  return (
+    <ProjectTemplate
+      description={entry.getIn(['data', 'description'])}
+      title={entry.getIn(['data', 'title'])}
+      seconddescription={entry.getIn(['data', 'seconddescription'])}
+      secondtitle={entry.getIn(['data', 'secondtitle'])}
+      thirddescription={entry.getIn(['data', 'thirddescription'])}
+      thirdtitle={entry.getIn(['data', 'thirdtitle'])}
+      mainimage={entry.getIn(['data', 'mainimage'])}
+      firstminiatureimage={entry.getIn(['data', 'firstminiatureimage'])}
+      secondminiatureimage={entry.getIn(['data', 'secondminiatureimage'])}
+    />
+  );
+};
 
 ProjectPreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func
-  }),
-  widgetFor: PropTypes.func
+  })
 };
 
 export default ProjectPreview;
