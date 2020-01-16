02aa8917b8d27b2161c242165500071c2a16baa9
Emil Schrøder <emilschroder@Emils-MacBook-Pro.local>
2020-01-16 18:31:02 +0100

    Add jumbotron to page

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import { navigate } from "gatsby-link";
-import Layout from "../../components/Layout";
-import Employees from "../../components/Employees";
+import React from 'react';
+import { navigate } from 'gatsby-link';
+import Layout from '../../components/Layout';
+import Employees from '../../components/Employees';
+import PageJumbotron from '../../components/PageJumbotron';
 
 function encode(data) {
   return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
 }
 
 export default class Index extends React.Component {
@@ -22,21 +23,28 @@ export default class Index extends React.Component {
   handleSubmit = e => {
     e.preventDefault();
     const form = e.target;
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
-        "form-name": form.getAttribute("name"),
+        'form-name': form.getAttribute('name'),
         ...this.state
       })
     })
-      .then(() => navigate(form.getAttribute("action")))
+      .then(() => navigate(form.getAttribute('action')))
       .catch(error => alert(error));
   };
 
   render() {
     return (
       <Layout>
+        <PageJumbotron
+          title={'Get in touch'}
+          image={''}
+          description={
+            'Drop a line and we’ll get back to you with lightning speed'
+          }
+        />
         <section className="section">
           <div className="container">
             <Employees />
@@ -54,50 +62,50 @@ export default class Index extends React.Component {
                 <input type="hidden" name="form-name" value="contact" />
                 <div hidden>
                   <label>
-                    Don’t fill this out:{" "}
+                    Don’t fill this out:{' '}
                     <input name="bot-field" onChange={this.handleChange} />
                   </label>
                 </div>
                 <div className="field">
-                  <label className="label" htmlFor={"name"}>
+                  <label className="label" htmlFor={'name'}>
                     Your name
                   </label>
                   <div className="control">
                     <input
                       className="input"
-                      type={"text"}
-                      name={"name"}
+                      type={'text'}
+                      name={'name'}
                       onChange={this.handleChange}
-                      id={"name"}
+                      id={'name'}
                       required={true}
                     />
                   </div>
                 </div>
                 <div className="field">
-                  <label className="label" htmlFor={"email"}>
+                  <label className="label" htmlFor={'email'}>
                     Email
                   </label>
                   <div className="control">
                     <input
                       className="input"
-                      type={"email"}
-                      name={"email"}
+                      type={'email'}
+                      name={'email'}
                       onChange={this.handleChange}
-                      id={"email"}
+                      id={'email'}
                       required={true}
                     />
                   </div>
                 </div>
                 <div className="field">
-                  <label className="label" htmlFor={"message"}>
+                  <label className="label" htmlFor={'message'}>
                     Message
                   </label>
                   <div className="control">
                     <textarea
                       className="textarea"
-                      name={"message"}
+                      name={'message'}
                       onChange={this.handleChange}
-                      id={"message"}
+                      id={'message'}
                       required={true}
                     />
                   </div>
