f7adf707b3f5b272b18544e93b5de72aaa0a05ff
Emil Schrøder <emilschroder@Emils-MacBook-Pro.local>
2020-01-18 15:12:19 +0100

    Add https

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -132,7 +132,7 @@ export class ContactPageTemplate extends React.Component {
           <div
             dangerouslySetInnerHTML={{
               __html: `
-            <iframe src="http://maps.google.com/maps?q=${mapiframe}&z=10&output=embed" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe src="https://maps.google.com/maps?q=${mapiframe}&z=10&output=embed" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 `
             }}
           />
