fa3c1899e524cfc8ef71188a627d44b7f63bb512
Emil Schrøder <emilschroder@Emils-MacBook-Pro.local>
2020-01-19 18:04:22 +0100

    Remove error

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -7,21 +7,23 @@ export default class ProductIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <PageJumbotron
-          title={'Products'}
-          image={'/img/products-grid2.jpg'}
-          description={
-            'Our base products are used in all our tailored solutions'
-          }
-        />
+        <div>
+          <PageJumbotron
+            title={'Products'}
+            image={'/img/products-grid2.jpg'}
+            description={
+              'Our base products are used in all our tailored solutions'
+            }
+          />
 
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <ProductRoll />
+          <section className="section">
+            <div className="container">
+              <div className="content">
+                <ProductRoll />
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </Layout>
     );
   }
