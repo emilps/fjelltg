305cbcec428b3aeda65b5f2618442a90bcd15667
Emil Schrøder <emilschroder@Emils-MacBook-Pro.local>
2020-01-24 23:15:43 +0100

    Design the contact form submittion page

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,5 +1,6 @@
-import React from "react";
-import Layout from "../../components/Layout";
+import React from 'react';
+import Layout from '../../components/Layout';
+import { Link } from 'gatsby';
 
 const Thanks = () => (
   <Layout>
@@ -7,12 +8,16 @@ const Thanks = () => (
       <div className="container">
         <div className="content">
           <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <p>You will be contacted as soon as possible!</p>
+          <p>In the meantime, you can check out our other projects</p>
+          <Link className="button" to={'/project'}>
+            Bring me there
+          </Link>
         </div>
       </div>
     </section>
   </Layout>
 );
 
-Thanks.displayname = "Thanks";
+Thanks.displayname = 'Thanks';
 export default Thanks;
