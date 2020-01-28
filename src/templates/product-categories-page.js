5e6e83c255b6542f814d425357fcc4a0f709da06
Emil Schrøder <emilschroder@Emils-MacBook-Pro.local>
2020-01-28 18:46:43 +0100

    Add content from cms

--- src/templates/product-categories-page.js
+++ src/templates/product-categories-page.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Layout from '../components/Layout';
 import ProductCategoryRoll from '../components/ProductCategoryRoll';
 import PageJumbotron from '../components/PageJumbotron';
 import SimpleCompanyQuote from '../components/SimpleCompanyQuote';
 import { graphql } from 'gatsby';
 
-export default class ProductCategoryIndexPage extends React.Component {
+class ProductsPageTemplate extends React.Component {
   render() {
+    const {
+      headertitle,
+      headerimage,
+      headerbyline,
+      productsquote
+    } = this.props;
+
     return (
-      <Layout>
+      <div>
         <PageJumbotron
-          title={'Products'}
-          image={'/img/products-grid2.jpg'}
-          description={'Category overview'}
+          title={headertitle}
+          image={headerimage}
+          description={headerbyline}
         />
-        <SimpleCompanyQuote
-          text={'Our base products are used in all our tailored solutions'}
-          isMainQuote={false}
-        />
+        <SimpleCompanyQuote text={productsquote} isMainQuote={false} />
         <section className="section remove-padding">
           <div className="container remove-margin">
             <div className="content is-fullwidth">
@@ -29,15 +34,50 @@ export default class ProductCategoryIndexPage extends React.Component {
           text={'Your partner for mass and heat transfer technology'}
           isMainQuote={true}
         />
-      </Layout>
+      </div>
     );
   }
 }
-/*
-export const pageQuery = graphql`
-  query ProductsPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "products-page" } }) {
+
+ProductsPageTemplate.propTypes = {
+  headerimage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  headertitle: PropTypes.string,
+  headerbyline: PropTypes.string,
+  productsquote: PropTypes.string
+};
+
+const ProductsPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark;
+
+  return (
+    <Layout>
+      <ProductsPageTemplate
+        headerimage={frontmatter.headerimage}
+        headertitle={frontmatter.headertitle}
+        headerbyline={frontmatter.headerbyline}
+        productsquote={frontmatter.productsquote}
+      />
+    </Layout>
+  );
+};
+
+ProductsPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object
+    })
+  })
+};
+
+export default ProductsPage;
+
+export const productsPageQuery = graphql`
+  query ProductsPage {
+    markdownRemark(
+      frontmatter: { templateKey: { eq: "product-categories-page" } }
+    ) {
       frontmatter {
+        productsquote
         headertitle
         headerbyline
         headerimage {
@@ -47,9 +87,7 @@ export const pageQuery = graphql`
             }
           }
         }
-        productsqoute
       }
     }
   }
 `;
-*/
